09945c9f847aa9e3a653a99770abfd2e47f390bd
whatev3r <113145908+whatev33r@users.noreply.github.com>
2023-08-23 15:11:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@
 from setuptools import setup
 
 PACKAGE_NAME = "ReconRaccoon"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
