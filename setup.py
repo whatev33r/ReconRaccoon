0ac44959a965793430752d1fa9ab8fefe45784bb
whatev3r <113145908+whatev33r@users.noreply.github.com>
2023-08-23 15:34:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import os
 from setuptools import setup
 
 PACKAGE_NAME = "ReconRaccoon"
-VERSION = "1.0.3"
+VERSION = "1.0.4"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         long_description_content_type="text/markdown",
         url="https://github.com/whatev33r/ReconRaccoon",
         entry_points={
-            "console_scripts": ["ReconRaccoon = ReconRaccoon.ReconRaccoon"],
+            "console_scripts": ["ReconRaccoon = ReconRaccoon.ReconRaccoon:main"],
         },
         install_requires=required,
         python_requires='>=3'
