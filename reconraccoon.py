c206d07e99b1efad6f3956525ba5ecab0f5c8f89
whatev3r <113145908+whatev33r@users.noreply.github.com>
2023-08-24 12:37:07 +0200

    Update reconraccoon.py

--- reconraccoon.py
+++ reconraccoon.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
-from reconraccoon.entry import main
+from ReconRaccoon.entry import main
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
     sys.exit(main())
