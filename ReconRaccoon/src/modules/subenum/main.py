05c087582efdd45b896966ae76143d1b90811a4e
cavoq <Davidstromberger99@gmx.de>
2023-10-19 12:47:29 +0200

    Refactor subenum and add sample list

--- ReconRaccoon/src/modules/subenum/main.py
+++ ReconRaccoon/src/modules/subenum/main.py
@@ -31,41 +31,58 @@ def __init__():
     main(args)
 
 
+class SubEnum:
+    def __init__(self, target: str, wordlist: str, output: str, verbose: bool):
+        self.target = target
+        self.wordlist = self.filter_wordlist(wordlist)
+        self.wildcard = self.get_wildcard()
+        self.output = output
+        self.verbose = verbose
+        self.ips = []
+        self.subs = []
+
+    def filter_wordlist(self, wordlist: str):
+        with open(wordlist) as file:
+            lst = file.readlines()
+        new_worlist = list(map(str.strip, lst))
+        new_worlist = list(dict.fromkeys(new_worlist))
+        new_worlist = [x.lower() for x in new_worlist]
+        return new_worlist
+
+    def get_wildcard(self):
+        wildcard = socket.gethostbyname(self.target)
+        return wildcard
+
+    def enumerate_subdomains(self):
+        print(
+            f'\r{cli.blue}[$]{cli.endc} Wildcard: {cli.blue}*{cli.endc}.{cli.bold}{self.target}{cli.endc}\t IP: {self.wildcard}')
+
+        for subdom in self.wordlist:
+            try:
+                dns = f'{subdom}.{self.target}'
+                ip = socket.gethostbyname(dns)
+
+                if ip != self.wildcard:
+                    print(
+                        f'\r{cli.green}[+]{cli.endc} Hostname: {subdom}.{self.target}\t IP: {ip}')
+                    self.ips.append(ip)
+                    self.subs.append(f'{subdom}.{self.target}')
+            except Exception as e:
+                if self.verbose:
+                    print(
+                        f'\r{cli.red}[-]{cli.endc} Hostname: {dns}\t Response: {e}')
+
+    def output_files(self):
+        if self.output:
+            with open('subs.txt', 'w') as f:
+                for line in self.subs:
+                    f.write(f'{line}\n')
+            with open('ips.txt', 'w') as f:
+                for line in self.ips:
+                    f.write(f'{line}\n')
+
+
 def main(args):
-    # Target
-    target = args.target
-    # Lists
-    ips = []
-    subs = []
-    # Filter Wordlist
-    f = open(args.wordlist)
-    lst = f.readlines()
-    new = list(map(str.strip, lst))
-    new = list(dict.fromkeys(new))
-    new = [x.lower() for x in new]
-    wildcard = socket.gethostbyname(target)
-    print(f'\r{cli.blue}[$]{cli.endc} Wildcard: {cli.blue}*{cli.endc}.{cli.bold}{target}{cli.endc}\t IP: {wildcard}')
-    # Main Loop
-    for subdom in new:
-        try:
-            dns = f'{subdom}.{target}'
-            ip = socket.gethostbyname(dns)
-            if ip != wildcard:
-                print(f'\r{cli.green}[+]{cli.endc} Hostname: {subdom}.{target}\t IP: {ip}')
-                ips.append(ip)
-                subs.append(f'{subdom}.{target}')
-            else:
-                pass
-        except Exception as E:
-            if args.verbose is True:
-                print(f'\r{cli.red}[-]{cli.endc} Hostname: {dns}\t Response: {E}')
-            else:
-                pass
-    # Generate Out Files
-    if args.output:
-        with open('subs.txt', 'w') as f:
-            for line in subs:
-                f.write(f'{line}\n')
-        with open('ips.txt', 'w') as f:
-            for line in ips:
-                f.write(f'{line}\n')
+    sub_enum = SubEnum(args.target, args.wordlist, args.output, args.verbose)
+    sub_enum.enumerate_subdomains()
+    sub_enum.output_files()
